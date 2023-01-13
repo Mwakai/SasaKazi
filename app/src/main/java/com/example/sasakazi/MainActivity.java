216b722d3a4adf53d5e43db1694adafd50a9ba00
Alvin Mwakai <mwakaimwambala@gmail.com>
2023-01-13 05:42:08 +0300

    Read data from the database

--- app/src/main/java/com/example/sasakazi/MainActivity.java
+++ app/src/main/java/com/example/sasakazi/MainActivity.java
@@ -1,15 +1,38 @@
 package com.example.sasakazi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editText;
+
+    Button button;
+    CardView cardView;
+    TextView textName, textEmail;
     FloatingActionButton floatingActionButton;
 
     @Override
@@ -17,11 +40,87 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        floatingActionButton=findViewById(R.id.fab);
-        floatingActionButton.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, AddUser.class);
-            startActivity(intent);
+        floatingActionButton = findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddUser.class);
+                startActivity(intent);
+            }
+        });
+
+        cardView = findViewById(R.id.card);
+        textName = findViewById(R.id.textName);
+        textEmail = findViewById(R.id.textEmail);
+        editText = findViewById(R.id.id);
+        button = findViewById(R.id.btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (TextUtils.isEmpty(editText.getText().toString())) {
+                    Toast.makeText(MainActivity.this, "Please enter an id", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                getUsers(editText.getText().toString());
+
+
+            }
         });
 
     }
+
+    private void getUsers(String s) {
+        String url = "http://192.168.100.140:84/Sasakazi/api.php";
+
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+
+        StringRequest request = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if (jsonObject.getString("name") == null) {
+                                Toast.makeText(MainActivity.this, "Emter A Valid ID", Toast.LENGTH_SHORT).show();
+                            }else {
+                                textName.setText(jsonObject.getString("name"));
+                                textEmail.setText(jsonObject.getString("email"));
+                                cardView.setVisibility(View.VISIBLE);
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "Fail to get User" + error, Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                // as we are passing data in the form of url encoded
+                // so we are passing the content type below
+                return "application/x-www-form-urlencoded; charset=UTF-8";
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+
+                Map<String, String> params = new HashMap<String, String>();
+
+                params.put("id", s);
+
+                return params;
+            }
+        };
+
+        queue.add(request);
+    }
+
 }
---
+++ app/src/main/java/com/example/sasakazi/model/Users.java
@@ -0,0 +1,26 @@
+package com.example.sasakazi.model;
+
+public class Users {
+
+    private int id;
+    private String name;
+    private String email;
+
+    public Users(int id, String name, String email) {
+        this.id= id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
